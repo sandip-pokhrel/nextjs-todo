f6c807f8d8d837e39e0a1911c59b72f479a28298
sandip-pokhrel <sandippokhrel.themegrill@gmail.com>
2023-01-10 17:39:06 +0545

    make starship popup

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ import { useEffect, useState } from "react";
 import { API } from "../utils/axios";
 import { AxiosResponse } from "axios";
 
-// TODO: use star wars people API to get the list of People which when clicked gives a list of starships they have
+// TODO: make a popup component to display the starship's Name
+// use star wars people API to get the list of People which when clicked gives a list of starships they have
 
 interface ITodo {
   title: string;
